2fbd911bb48a6e49f830aa1504a87aace1af3350
jinyang <jyjz2008@sjtu.edu.cn>
2015-01-28 20:15:23 +0800

    north

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
 #include <cstdio>
 
-using namespace std;
+const int N = 1050;
+
+bool record[N];
 
 int main(){
-	
+	int c;
+	int n;
+	while(scanf("%d %d", &c, &n) != EOF){
+		memset(record, 0, c);
+		int max = -1;
+		int temp;
+		for(int i = 0;i < n;i++){
+			scanf("%d", &temp);
+			for(int j = 0;j <= max;j++){
+				if(record[j] == true)
+			}
+		}
+	}
 	return 0;
 }
